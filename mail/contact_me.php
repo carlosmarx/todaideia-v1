e17dc5778750648278fc104f95d4cbea584fdefd
Carlos Marx <carlosmarx27@gmail.com>
2017-01-06 15:47:33 -0300

    Adjust charset

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,4 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -19,9 +18,9 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'contato@todaideia.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato - Formulário do site:  $name";
-$email_body = "Você recebeu uma nova mensagem do formulário de contato do site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nFone: $phone\n\nMenssagem:\n$message";
+$email_body = "Você recebeu uma nova mensagem do formulário de contato do site.\n\n"."Aqui estão os detalhes: <br /> Nome: $name <br /><br /> Email: $email_address <br /><br /> Fone: $phone <br /><br /> Menssagem: <br />$message";
 $headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers = "Content-Type: text/html; charset=\"UTF-8\"\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "Content-Type: text/html; charset=\"UTF-8\"\n"; // This is encode charset type to utf-8
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
