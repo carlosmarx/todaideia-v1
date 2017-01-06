838de6ad9091614f2bb8f4668a9c758ca607e4ce
Carlos Marx <carlosmarx27@gmail.com>
2017-01-06 11:34:59 -0300

    Alterar email para o todaideia.com

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'contato@todaideia.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'contato@todaideia.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato - Formul&aacute;rio do site:  $name";
 $email_body = "Voc&ecirc; recebeu uma nova mensagem do formul&aacute;rio de contato do site.\n\n"."Aqui est&atilde;o os detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nFone: $phone\n\nMenssagem:\n$message";
 $headers = "De: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
